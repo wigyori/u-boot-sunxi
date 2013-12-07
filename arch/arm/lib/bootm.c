7b824ca7f30137b6af0241f955888225e8cce94e
Marc Zyngier <marc.zyngier@arm.com>
2013-12-07 11:19:12 +0000

    ARM: HYP/non-sec: allow relocation to secure RAM
    The current non-sec switching code suffers from one major issue:
    it cannot run in secure RAM, as a large part of u-boot still needs
    to be run while we're switched to non-secure.
    
    This patch reworks the whole HYP/non-secure strategy by:
    - making sure the secure code is the *last* thing u-boot executes
      before entering the payload
    - performing an exception return from secure mode directly into
      the payload
    - allowing the code to be dynamically relocated to secure RAM
      before switching to non-secure.
    
    This involves quite a bit of horrible code, specially as u-boot
    relocation is quite primitive.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/lib/bootm.c
+++ arch/arm/lib/bootm.c
@@ -20,6 +20,7 @@
 #include <libfdt.h>
 #include <fdt_support.h>
 #include <asm/bootm.h>
+#include <asm/secure.h>
 #include <linux/compiler.h>
 
 #if defined(CONFIG_ARMV7_NONSEC) || defined(CONFIG_ARMV7_VIRT)
@@ -185,19 +186,6 @@ static void setup_end_tag(bd_t *bd)
 
 __weak void setup_board_tags(struct tag **in_params) {}
 
-static void do_nonsec_virt_switch(void)
-{
-#if defined(CONFIG_ARMV7_NONSEC) || defined(CONFIG_ARMV7_VIRT)
-	if (armv7_switch_nonsec() == 0)
-#ifdef CONFIG_ARMV7_VIRT
-		if (armv7_switch_hyp() == 0)
-			debug("entered HYP mode\n");
-#else
-		debug("entered non-secure state\n");
-#endif
-#endif
-}
-
 /* Subcommand: PREP */
 static void boot_prep_linux(bootm_headers_t *images)
 {
@@ -264,8 +252,13 @@ static void boot_jump_linux(bootm_headers_t *images, int flag)
 		r2 = gd->bd->bi_boot_params;
 
 	if (!fake) {
-		do_nonsec_virt_switch();
+#if defined(CONFIG_ARMV7_NONSEC) || defined(CONFIG_ARMV7_VIRT)
+		armv7_init_nonsec();
+		secure_ram_addr(_do_nonsec_entry)(kernel_entry,
+						  0, machid, r2);
+#else
 		kernel_entry(0, machid, r2);
+#endif
 	}
 }
 
